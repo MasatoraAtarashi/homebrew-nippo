1c7d648f68cb1932410cbdda1f2b6ba4f727228c
Masatora Atarashi <t17023ma@sfc.keio.ac.jp>
2021-05-08 18:40:30 +0900

    Update nippo-generator.rb

--- nippo-generator.rb
+++ nippo-generator.rb
@@ -1,7 +1,7 @@
 class NippoGenerator < Formula
   desc "自動日報ジェネレーター"
   homepage "https://github.com/MasatoraAtarashi/nippo-generator"
-  url "https://github.com/MasatoraAtarashi/nippo-generator/releases/download/v0.0.3/nippo"
+  url "https://github.com/MasatoraAtarashi/nippo-generator/releases/download/v0.0.3/nippo-generator"
   sha256 "1c17c785899eca9209f765680f2fd2e8a33515b3b573f772074ca45f1aaef876"
   license "Apache-2.0"
 
