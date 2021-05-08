a238e035d549c43003a2e9a4283da6dd405d4905
Masatora Atarashi <t17023ma@sfc.keio.ac.jp>
2021-05-08 18:30:53 +0900

    Update nippo-generator.rb

--- nippo-generator.rb
+++ nippo-generator.rb
@@ -12,29 +12,6 @@ class NippoGenerator < Formula
   # depends_on "cmake" => :build
 
   def install
-    print buildpath
-    ENV["GOPATH"] = buildpath
-    nippo_path = buildpath/"src/github.com/MasatoraAtarashi/nippo-generator/"
-    nippo_path.install buildpath.children
-
-    # ソースコードのbuildPathに移動した後、depコマンドで依存パッケージを取得し、buildを行う。
-    cd nippo_path do
-      system "dep", "ensure", "-vendor-only"
-      system "go", "build"
-      bin.install "imemo"
-    end
-  end
-
-  test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! For Homebrew/homebrew-core
-    # this will need to be a test that verifies the functionality of the
-    # software. Run the test with `brew test nippo-generator`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system "#{bin}/program", "do", "something"`.
-    system "false"
+    bin.install 'nippo'
   end
 end
