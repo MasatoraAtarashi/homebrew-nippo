6521a6f49e90596977d8a7fa82a6a3e83ceca94e
Masatora Atarashi <t17023ma@sfc.keio.ac.jp>
2021-05-08 19:39:19 +0900

    Update nippo-generator.rb

--- nippo-generator.rb
+++ nippo-generator.rb
@@ -1,11 +1,8 @@
-# Documentation: https://docs.brew.sh/Formula-Cookbook
-#                https://rubydoc.brew.sh/Formula
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
-class NippoGenerator < Formula
+class Nippo < Formula
   desc "自動日報ジェネレーター"
   homepage ""
-  url "https://github.com/MasatoraAtarashi/nippo-generator/archive/refs/tags/v0.0.3.tar.gz"
-  sha256 "1333f7b19df6a687554dde226996386c62d6a0231adcb7a0b1c1cc8ae7f6bbd8"
+  url "https://github.com/MasatoraAtarashi/nippo/archive/refs/tags/v0.0.3.tar.gz"
+  sha256 "6e397c03469e24dae5c2fe7a18566ede403f793009b6bd426b74d5adda73a4f6"
   license "Apache-2.0"
 
   # depends_on "cmake" => :build
@@ -19,7 +16,7 @@ class NippoGenerator < Formula
     #
     # This test will fail and we won't accept that! For Homebrew/homebrew-core
     # this will need to be a test that verifies the functionality of the
-    # software. Run the test with `brew test nippo-generator`. Options passed
+    # software. Run the test with `brew test nippo`. Options passed
     # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
     #
     # The installed folder is not in the path, so use the entire path to any
